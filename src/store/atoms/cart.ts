a0fa6457db98112aa775fc8a823835f8377c90a1
Samith Fernando <samith.fernando@amusedgroup.com>
2024-08-05 20:38:34 +0530

    set cart items into local storage

--- src/store/atoms/cart.ts
+++ src/store/atoms/cart.ts
@@ -1,11 +1,13 @@
 import { CartItem } from "@/components/add-to-cart-btn";
-import { atomWithImmer } from "jotai-immer";
+import { withImmer } from "jotai-immer";
 import { useAtomsDebugValue } from "jotai-devtools/utils";
+
+import { atomWithStorage } from "jotai/utils";
 
 export const DebugAtoms = () => {
   useAtomsDebugValue();
   return null;
 };
 
-export const cartAtom = atomWithImmer<CartItem[]>([]);
-cartAtom.debugLabel = "cart";
+export const cartAtom = withImmer(atomWithStorage<CartItem[]>("cartItems", []));
+cartAtom.debugLabel = "cartItems";
